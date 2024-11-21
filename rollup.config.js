9b74635e377e7fe0ffa0dcd29d45f55010569228
markliu2013 <markliu2013@gmail.com>
2024-11-21 09:25:15 +0800

    save

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,25 @@
 export default {
     input: 'src/index.js',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'iife'
-    }
+    output: [
+        {
+            //打包格式
+            format: 'es',
+            //打包后文件名
+            entryFileNames: '[name].mjs',
+            //让打包目录和我们目录对应
+            preserveModules: true,
+            exports: 'named',
+            dir: './dist/es'
+        },
+        {
+            //打包格式
+            format: 'cjs',
+            //打包后文件名
+            entryFileNames: '[name].js',
+            //让打包目录和我们目录对应
+            preserveModules: true,
+            exports: 'named',
+            dir: './dist/lib'
+        }
+    ]
 }
